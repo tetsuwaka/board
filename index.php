8c923b6302bbbb5169b48ad976f2ed507c31109f
tetsuwaka <tetsuwaka@gmail.com>
2012-06-14 01:37:32 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>test</title>
+</head>
+
+<body>
+<h1>TESTAAAAA</h1>
+<?php echo '今日は晴れ'; ?>
+</body>
+</html>
