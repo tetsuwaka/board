2b16dae55922374908ddaf42512859c59608023d
hiroki_sakaji <hiroki_sakaji@h-sakaji-so.office.dwango.co.jp>
2012-06-18 19:36:00 +0900

    ほぼ完成

--- lib/dbaccess.php
+++ lib/dbaccess.php
@@ -3,13 +3,27 @@
 
 # PDOを利用したDBへのアクセス
 function db_connect () {
-  $dsn = 'mysql:dbname=board;host=localhost';
-  $user = 'root';
-  //$pass = 'tessan';
-  $pass = '09023';
+
+  //$pass = '09023';
   $pdo = new PDO($dsn, $user, $pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $pdo;
+}
+
+
+# 管理者アクセス
+function Authenticator($id , $pass) {
+
+  $db = db_connect();
+  $sql = "SELECT id, pass from member where id = :id";
+  $params[':id'] = $id;
+  $stmt = $db->prepare($sql);
+  $stmt->execute($params);
+  $member= $stmt->fetch();
+  if ($id == $member['id'] && sha1($pass) == $member['pass']) {
+    return true;
+  }
+  return false;
 }
 
 ?>
