ef50fdc252ddf2df85e45012ce8d41dfd8820975
sakaji <sakaji@cyclone>
2012-07-08 11:57:50 +0900

    変更

--- models/DefaultMemberRepository.php
+++ models/DefaultMemberRepository.php
@@ -4,6 +4,6 @@ class DefaultMemberRepository extends DbRepository {
 
     public function makeDefaultUser($userid, $pass) {
         $sql = "INSERT INTO member VALUES (:userid, :pass)";
-        return $this->execute($sql, array(':userid' => $userid, 'pass' => $pass));
+        return $this->execute($sql, array(':userid' => $userid, 'pass' => sha1($pass)));
     }
 }
