7b9cd00153f2271ede82108dcbdefaa840104f2a
hiroki_sakaji <hiroki_sakaji@h-sakaji-so.office.dwango.co.jp>
2012-07-11 11:23:13 +0900

    commit

--- models/MemberRepository.php
+++ models/MemberRepository.php
@@ -8,6 +8,6 @@ class MemberRepository extends DbRepository {
     
     public function fetchByUserId ($userid) {
         $sql = "SELECT * from member where id = :userid";
-        return $this->execute($sql, array(':userid' => $userid));
+        return $this->fetch($sql, array(':userid' => $userid));
     }
 }
